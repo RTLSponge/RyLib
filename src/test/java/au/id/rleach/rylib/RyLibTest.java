1cdef8e50752939ae0884a9878151e91eb1a9511
Ryan Leach <rleach@rleach.id.au>
2018-06-13 13:17:12 +0930

    Upgrade Gradle4.8, Testing JUnit 5, Reproducible Builds

---
+++ src/test/java/au/id/rleach/rylib/RyLibTest.java
@@ -0,0 +1,16 @@
+package au.id.rleach.rylib;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RyLibTest {
+
+    @Test
+    void onServerStart() {
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    void onPlayerJoin() {
+    }
+}
