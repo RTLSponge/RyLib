4680b27d3a9b21b9674f9147e4c28e64fa541d31
Ryan Leach <rleach@rleach.id.au>
2018-06-13 18:58:18 +0930

    Fixed scopes dependency

--- src/main/java/au/id/rleach/rylib/ScopeModule.java
+++ src/main/java/au/id/rleach/rylib/ScopeModule.java
@@ -1,8 +1,13 @@
 package au.id.rleach.rylib;
 
 import com.google.inject.AbstractModule;
+import com.mastfrog.giulius.scope.ReentrantScope;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.cause.EventContextKeys;
+import org.spongepowered.api.world.World;
 
 public class ScopeModule {
+    ReentrantScope scope;
     class MyModule extends AbstractModule {
         public void configure() {
             ReentrantScope scope = new ReentrantScope();
@@ -11,18 +16,25 @@ public class ScopeModule {
             // there is no getting around explitly binding classes, but
             // you can provide a way to pass in an array easily enough:
 
-            scope.bindTypes(binder(), FooRequest.class, FooResponse.class, ...);
+            scope.bindTypes(binder(), World.class);
         }
     }
 
     public class TheApp {
 
-        public void onRequest(FooRequest req) {
-            FooResponse resp = new FooResponse();
-            try (AutoCloseable ac = scope.enter(req, resp)) {
+        public void contextually(Runnable run) {
+            Sponge.getGame().getCauseStackManager()
+                    .getContext(EventContextKeys.)
+            try (AutoCloseable ac = scope.enter()) {
                 // Anything with a Provider<FooRequest>, etc. will
                 // get the passed request
             }
+            run
+        }
+
+        public void onRequest(FooRequest req) {
+            FooResponse resp = new FooResponse();
+
         }
     }
 }
